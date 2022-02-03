5c84bdc2d075e2f0fb5d55b53a2c4f2a78db4d64
kahnu <kanhus082@gmail.com>
2022-02-03 17:52:10 +0530

    home

---
+++ home.php
@@ -0,0 +1,7 @@
+<html>
+<a href="Online_notice_board/index.php">Online notice board</a>
+<a href="online-notes-sharing/index.php">online note sharing</a>
+<a  href="OnlineExamSystem/index.php">online exam system</a>
+<a  href="Resume/index.php">Sudent Resume</a>
+<a  href="Task-Management-master/index.php">online note sharing</a>
+</html>
