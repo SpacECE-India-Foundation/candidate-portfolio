bdc6707c2eb27883803310f7152c9d1a3edd64a3
kahnu <kanhus082@gmail.com>
2022-02-03 22:35:55 +0530

    depali

--- home.php
+++ home.php
@@ -1,6 +1,9 @@
 <html>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<div class="container bg-secondary">
+<body class="bg-light">
+    
+
+<div class="container ">
 <div class="row ml-3 mt-5">
 <div class="col">
 <a class="btn btn-primary" href="Online_notice_board/index.php">Online notice board</a>
@@ -19,12 +22,16 @@
     <a class="btn btn-primary" href="Task-Management-master/index.php">online note sharing</a></div>
 </div>
 
+<div class="row ml-3 mt-5">
+<div class="col ml-3">
+    <a class="btn btn-primary" href="student_result/srms/index.php">student result</a></div>
+</div>
 </div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
-
+</body>
 
 </html>
