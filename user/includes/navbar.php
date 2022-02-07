cb0026b00efe9076c7c31d4aa81bbeff6b19575d
vkm94 <varunmanila89@gmail.com>
2022-02-08 02:39:22 +0530

    updated

---
+++ user/includes/navbar.php
@@ -0,0 +1,24 @@
+ <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"">
+			<div class="container">
+
+
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle Navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php"></a>
+				</div>
+
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                    	  <h1 style="color:white;margin-left:500px;text-size:20px"><b><center>Well come in SpacECE-Candidate Portal</b></center></h1>
+
+
+
+                    </ul>
+				</div>
+			</div>
+	    </nav>
