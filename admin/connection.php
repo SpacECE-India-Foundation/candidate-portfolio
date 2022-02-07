dd63cb653e5f81f637865ec03b3658bc2a6d5b86
vkm94 <varunmanila89@gmail.com>
2022-02-08 02:46:53 +0530

    updated

--- admin/connection.php
+++ admin/connection.php
@@ -1,3 +1,4 @@
 <?php
-$conn=mysqli_connect("localhost","root","","candidate_portal");
+//$conn=mysqli_connect("localhost","root","","candidate_portal");
+$conn = mysqli_connect("http://3.109.14.4/","ostechnix","Password123#@!","candidate_portal" ) or die ("error" . mysqli_error($conn));
 ?>
--- admin/login.php
+++ admin/login.php
@@ -1,5 +1,5 @@
 <?php
-	include('../connection.php');
+	include('connection.php');
 	session_start();
 	extract($_POST);
 	if(isset($login))
