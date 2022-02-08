aa39a4641121e562a9c0ef27830ee17d7efcac8d
vkm94 <varunmanila89@gmail.com>
2022-02-08 16:28:40 +0530

    updated

--- user/signup.php
+++ user/signup.php
@@ -25,8 +25,8 @@ $validated_data = $gump->run($_POST);
 $conn = new mysqli('3.109.14.4', 'ostechnix', 'Password123#@!', 'candidate_portal');
 
 // Check connection
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
     exit();
 }
 
