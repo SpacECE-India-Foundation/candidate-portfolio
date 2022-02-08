5dc0732f0037903882059b68431559ce2f4f51ff
vkm94 <varunmanila89@gmail.com>
2022-02-08 15:24:26 +0530

    updated

--- user/includes/connection.php
+++ user/includes/connection.php
@@ -1,4 +1,12 @@
 <?php
 //$conn = mysqli_connect("localhost","root","","candidate_portal" ) or die ("error" . mysqli_error($conn));
-$conn = mysqli_connect("http://3.109.14.4/","ostechnix","Password123#@!","candidate_portal" ) or die ("error" . mysqli_error($conn));
+//$conn = mysqli_connect("http://3.109.14.4/","ostechnix","Password123#@!","candidate_portal" ) or die ("error" . mysqli_error($conn));
+$conn = new mysqli('3.109.14.4', 'ostechnix', 'Password123#@!', 'candidate_portal');
+
+// Check connection
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
 ?>
--- user/login.php
+++ user/login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include './includes/connection.php';
  ?>
 <?php include './includes/header.php';?>
 <?php  include 'includes/navbar.php';?>
