6737c30864e6ed179ef83adc91436ab20c347b54
vkm94 <varunmanila89@gmail.com>
2022-02-08 14:35:14 +0530

    updated

--- admin/connection.php
+++ admin/connection.php
@@ -1,6 +1,6 @@
 <?php
 //$conn=mysqli_connect("localhost","root","","candidate_portal");
-$conn = mysqli_connect("http://3.109.14.4/","ostechnix","Password123#@!","candidate_portal" ) or die ("error" . mysqli_error($conn));
+$conn = mysqli_connect("3.109.14.4","ostechnix","Password123#@!","candidate_portal" ) or die ("error" . mysqli_error($conn));
 
 
 
