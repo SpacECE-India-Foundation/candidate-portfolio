b9fd851045660a0bb4c541a20ff817528e2488c8
vkm94 <varunmanila89@gmail.com>
2022-02-08 14:14:11 +0530

    updated

--- user/login.php
+++ user/login.php
@@ -1,61 +1,61 @@
 <?php
 session_start();
- include 'includes/connection.php';?>
-<?php include 'includes/header.php';?>
-<?php include 'includes/navbar.php';?>
+ //include 'includes/connection.php';?>
+<?php // include 'includes/header.php';?>
+<?php // include 'includes/navbar.php';?>
 
 <?php
 
-if (isset($_POST['login'])) {
-  $pass1 =$_POST['pass'];
-  $username  = $_POST['user'];
-  $password =md5($pass1);
-  mysqli_real_escape_string($conn, $username);
-  mysqli_real_escape_string($conn, $password);
-$query = "SELECT * FROM user WHERE uname = '$username'";
+// if (isset($_POST['login'])) {
+//   $pass1 =$_POST['pass'];
+//   $username  = $_POST['user'];
+//   $password =md5($pass1);
+//   mysqli_real_escape_string($conn, $username);
+//   mysqli_real_escape_string($conn, $password);
+// $query = "SELECT * FROM user WHERE uname = '$username'";
 
-$result = mysqli_query($conn , $query) or die (mysqli_error($conn));
-if (mysqli_num_rows($result) > 0) {
-  while ($row = mysqli_fetch_array($result)) {
-    $id = $row['uid'];
-    $user = $row['uname'];
-    $pass = $row['upass'];
+// $result = mysqli_query($conn , $query) or die (mysqli_error($conn));
+// if (mysqli_num_rows($result) > 0) {
+//   while ($row = mysqli_fetch_array($result)) {
+//     $id = $row['uid'];
+//     $user = $row['uname'];
+//     $pass = $row['upass'];
    
-    $name = $row['name'];
-    $email = $row['uname'];
-    $role= $row['role'];
-    $course = $row['course'];
+//     $name = $row['name'];
+//     $email = $row['uname'];
+//     $role= $row['role'];
+//     $course = $row['course'];
   
-    if (($password=== $pass )) {
-      $_SESSION['id'] = $id;
-      $_SESSION['username'] = $username;
-      $_SESSION['name'] = $name;
-      $_SESSION['email']  = $email;
-      $_SESSION['user']=$email;
-      $_SESSION['role'] = $role;
-      $_SESSION['course'] = $course;
-      $_SESSION['SESS_MEMBER_ID']=$id;
-      $_SESSION['SESS_FIRST_NAME']=$row['uname'];
-	    $_SESSION['SESS_LAST_NAME']=$row['name'];
-      $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
-    foreach($system as $k => $v){
-      $_SESSION['system'][$k] = $v;
-    }
-      header('location: ../index.php');
-    }
-    else {
-      echo "<script>alert('invalid username/password');
-     window.location.href= 'login.php';</script>";
+//     if (($password=== $pass )) {
+//       $_SESSION['id'] = $id;
+//       $_SESSION['username'] = $username;
+//       $_SESSION['name'] = $name;
+//       $_SESSION['email']  = $email;
+//       $_SESSION['user']=$email;
+//       $_SESSION['role'] = $role;
+//       $_SESSION['course'] = $course;
+//       $_SESSION['SESS_MEMBER_ID']=$id;
+//       $_SESSION['SESS_FIRST_NAME']=$row['uname'];
+// 	    $_SESSION['SESS_LAST_NAME']=$row['name'];
+//       $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
+//     foreach($system as $k => $v){
+//       $_SESSION['system'][$k] = $v;
+//     }
+//       header('location: ../index.php');
+//     }
+//     else {
+//       echo "<script>alert('invalid username/password');
+//      window.location.href= 'login.php';</script>";
 
-    }
-  }
-}
-else {
-     echo "<script>alert('invalid username/password');
-     window.location.href= 'login.php';</script>";
+//     }
+//   }
+// }
+// else {
+//      echo "<script>alert('invalid username/password');
+//      window.location.href= 'login.php';</script>";
 
-    }
-}
+//     }
+// }
 ?>
 
 
