4b7ac4533ca3155300860e9680257146fb3f498b
vkm94 <varunmanila89@gmail.com>
2022-02-10 10:58:14 +0530

    updated

--- OnlineExamSystem-PHP/index.php
+++ OnlineExamSystem-PHP/index.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION['login_id']))
+header('location:../user/login.php');
+include 'db_connect.php';
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
