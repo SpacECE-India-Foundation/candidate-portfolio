e2c8899e693c1c7e274fa52e8e77f916d15b13e7
vkm94 <varunmanila89@gmail.com>
2022-02-12 12:18:15 +0530

    updated

--- online-notes-sharing/dashboard/includes/connection.php
+++ online-notes-sharing/dashboard/includes/connection.php
@@ -1,3 +1,10 @@
 <?php
-$conn = mysqli_connect("localhost","root","","candidate_portal" ) or die ("error" . mysqli_error($conn));
+//$conn = mysqli_connect("localhost","root","","candidate_portal" ) or die ("error" . mysqli_error($conn));
+$con = new mysqli('3.109.14.4', 'ostechnix', 'Password123#@!', 'candidate_portal');
+
+// Check connection
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: " . $con->connect_error;
+    exit();
+}
 
