488d92c0757d65491c6d92477c264eaea47e10a8
vkm94 <varunmanila89@gmail.com>
2022-02-21 00:39:10 +0530

    updated

--- Task-Management-master/db_connect.php
+++ Task-Management-master/db_connect.php
@@ -1,5 +1,12 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','candidate_portal')or die("Could not connect to mysql".mysqli_error($con));
+//$conn= new mysqli('localhost','root','','candidate_portal')or die("Could not connect to mysql".mysqli_error($con));
+$conn = mysqli_connect("3.109.14.4","ostechnix","Password123#@!","candidate_portal" ) or die ("error" . mysqli_error($conn));
+
+// Check connection
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
+}
 
 ?>
