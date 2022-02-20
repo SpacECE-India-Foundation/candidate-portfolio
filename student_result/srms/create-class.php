896bb9d6c03519d571b5dd3cafb534e5a387a3c2
vkm94 <varunmanila89@gmail.com>
2022-02-21 00:51:18 +0530

    updated

--- student_result/srms/create-class.php
+++ student_result/srms/create-class.php
@@ -17,7 +17,6 @@ $query = $dbh->prepare($sql);
 $query->bindParam(':classname',$classname,PDO::PARAM_STR);
 $query->bindParam(':classnamenumeric',$classnamenumeric,PDO::PARAM_STR);
 $query->bindParam(':section',$section,PDO::PARAM_STR);
-
 $query->execute();
 $lastInsertId = $dbh->lastInsertId();
 if($lastInsertId)
