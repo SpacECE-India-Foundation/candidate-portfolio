2e9e538626e203d5e2625950eae79f77e9905274
vkm94 <varunmanila89@gmail.com>
2022-02-21 10:57:46 +0530

    updated

--- chatbot/initialize.php
+++ chatbot/initialize.php
@@ -3,8 +3,8 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 if(!defined('base_url')) define('base_url','http://localhost/chatbot/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_SERVER')) define('DB_SERVER',"3.109.14.4");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"ostechnix");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Password123#@!");
 if(!defined('DB_NAME')) define('DB_NAME',"candidate_portal");
 ?>
