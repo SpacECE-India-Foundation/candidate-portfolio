ea866488bc87ae4c21846968625f0035abd44721
sachin-mohite <sachin.mohite@gmail.com>
2022-02-27 16:55:41 +0530

    Update 30daysnotification.php

--- OnlineExamSystem-PHP/30daysnotification.php
+++ OnlineExamSystem-PHP/30daysnotification.php
@@ -1,29 +1,54 @@
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<script>
-// Do this instead
-setTimeout(function() {
-  console.log('date');
-  //  use as simple as
+<?php
+session_start();
+include 'dbConnection.php';
 
-  const d = new Date();
-d.getDate();
-var y=new Date();
-if(x=30){
-    $.ajax({
-    url:"exammail.php",
-    type: "POST",
-    data: {y:y},
-    success: function(data, textStatus, jqXHR) {
-        alert(data);
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-        alert('Error occurred!');
+//Fetch list of candidates compliting 30 days from joining date
+$queryGetAllCandidates = "SELECT * from user where DATEDIFF(CURRENT_DATE(), joindate)>30;";//Todo: Below 60
+$resultGetAllCandidates = mysqli_query($con, $queryGetAllCandidates)or die('Error231');
 
-    }
+if(mysqli_num_rows($resultGetAllCandidates) > 0){
 
-}); 
-}
-}, 500);
-</script>
+    while ($rowGetAllCandidates = mysqli_fetch_array($resultGetAllCandidates)) {
+    
+        //Fetching candidates details
+        $candidateId = $rowGetAllCandidates['uid'];
+        $candidateName = $rowGetAllCandidates['uname'];
+        $candidateDepartment = $rowGetAllCandidates['department'];
+        $candiateEmail = $rowGetAllCandidates['uemail'];
+        
+        echo "Candidate Name:".$candidateName."; department:".$candidateDepartment;
+
+        //Randomly fetch assessment departmentwise
+        $queryRandAssessment = "SELECT * FROM assessment where department = '$candidateDepartment' ORDER BY RAND() LIMIT 1";
+        $resultRandAssessment = mysqli_query($con, $queryRandAssessment)or die('Error231');
+        $rowRandAssessment = mysqli_fetch_array($resultRandAssessment);
+
+        //Assign the assignment to candidates
+        if (mysqli_num_rows($resultRandAssessment) > 0) {
+            
+            //Fetching assessment details
+            $assesmentId = $rowRandAssessment['assessmentId'];
+            $assessmentDescription = $rowRandAssessment['assessmentDescription'];
+            $assesmentTitle = $rowRandAssessment['assessmentTitle'];
+            $assesmentDepartment = $rowRandAssessment['department'];
+
+            echo "; Assessment:".$assesmentId."\n";
+
+            $queryAllocateAssessment = "UPDATE user SET assesment1id = '$assesmentId' WHERE uid = '$candidateId'";
+            if(mysqli_query($con, $queryAllocateAssessment) or die('Error231')){
+                echo "\nAssessment allocated successfully to..".$candidateName;
+
+                //email to candidate
+                $emailSubject = "[SpacECE Internship] Assessment";
+                $emailBody = "Your Assessment is as following..\nTitle:".$assesmentTitle."\nAssessment Description:".$assessmentDescription;
+                echo "\nEmail Subject:".$emailSubject;
+                echo "\nEmail body:".$emailBody;
+                mail($candiateEmail, $emailSubject, $emailBody);
+            }
+
+        }//if (mysqli_num_rows($resultRandAssessment) > 0) {
+        else{
+            echo "Assessment: Nothing";
+        }
+    }//while ($row = mysqli_fetch_array($resultRandAssessment)) {
+}//if(mysqli_num_rows($resultGetAllCandidates) > 0){
