9e47c8c2f5541e633880149c795642ffd45fee1a
vkm94 <varunmanila@gmail.com>
2022-03-01 18:42:22 +0530

    updated

--- OnlineExamSystem-PHP/exammail.php
+++ OnlineExamSystem-PHP/exammail.php
@@ -4,18 +4,17 @@
 	$sql=mysqli_query($con,"select * from user join notice where user.uname=notice.user");
 	while($r=mysqli_fetch_array($sql))
 {
-	 print_r ($r );
-     sendmailto($r['uname'],$r['subject'],$r['description'],$r['Date'])
+	// print_r ($r );
+     sendmailto($r['uname'],$r['subject'],$r['description'],$r['Date']);
 
 	}
-    sendmailto("user","My subject",$msg){
-        // the message
-      
-
-        // use wordwrap() if lines are longer than 70 characters
-        
-        
-        // send email
-        mail("user","My subject",$msg);
-    }
+    sendmailto($uname,$subject,$msg){
+    
+        if(mail($uname,$subject,$msg)){
+            echo "SENT";
+        }else{
+            echo "Error";
+        }
+    /}
+}
 			?>
