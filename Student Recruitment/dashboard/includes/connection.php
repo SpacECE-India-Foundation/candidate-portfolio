0714bbf5be59fa9b3e128b83ac7f88f0d3ea50ac
vkm94 <varunmanila@gmail.com>
2022-03-07 22:26:36 +0530

    updated

--- Student Recruitment/dashboard/includes/connection.php
+++ Student Recruitment/dashboard/includes/connection.php
@@ -1,3 +1,4 @@
 <?php
-$conn = mysqli_connect("localhost","root","","candidate_portal" ) or die ("error" . mysqli_error($conn));
+//$conn = mysqli_connect("localhost","root","","candidate_portal" ) or die ("error" . mysqli_error($conn));
+$conn= new mysqli('3.109.14.4', 'ostechnix', 'Password123#@!', 'candidate_portal' )or die("Could not connect to mysql".mysqli_error($con));
 ?>
