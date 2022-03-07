87537b05d3fa0a94cd4a81fede99ba8738bd6714
vkm94 <varunmanila@gmail.com>
2022-03-07 17:13:57 +0530

    updated

--- Student Recruitment/admin/admin_class.php
+++ Student Recruitment/admin/admin_class.php
@@ -198,8 +198,8 @@ Class Action {
 			$save = $this->db->query("UPDATE application set ".$data." where id=".$id);
 		}
 		if($save)
-		$headers = "MIME-Version: 1.0" . "\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	//	$headers = "MIME-Version: 1.0" . "\r\n";
+	//	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 		
 		//$message="Please find your assignment below
 	    //Assignment: Create an api";
