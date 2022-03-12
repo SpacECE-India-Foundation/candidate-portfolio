c7e686c514ea03593e398d24fd0cb575d7f8ee5a
vkm94 <varunmanila@gmail.com>
2022-03-12 20:54:48 +0530

    updated

--- user/includes/header.php
+++ user/includes/header.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
 
     <link rel="stylesheet" href="../css/loginstyle.css">
-	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	
 </head>
 <body>
